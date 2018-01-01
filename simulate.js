152dda62f3137cac27b3905a382e7b7d2999bc54
shivshank <shivshank@users.noreply.github.com>
2018-01-01 17:36:33 -0500

    Calculate orbital elements (though it is buggy!)

--- simulate.js
+++ simulate.js
@@ -4,6 +4,7 @@
 
 // all base parameters approximate, based on ISS and Earth (or wherever Google sourced its query
 // responses from (since I'm lazy))
+// inclination of ISS orbit is not based on anything, I just picked a vector with magnitude 8000
 
 var model = {
     planet: {
@@ -18,7 +19,7 @@ var model = {
         // ISS is roughly ~400km away from a planet that has radius 6000km
         pos: new THREE.Vector3(6410000, 0, 0),
         // traveling at 7,500 m/s
-        vel: new THREE.Vector3(0, 0, 8000),
+        vel: new THREE.Vector3(0, -1, 1).normalize().multiplyScalar(9000),
     }
 };
 
@@ -69,4 +70,118 @@ function step_model(dt) {
         remaining -= dt;
     }
     return remaining / time_scale;
+}
+
+var KeplerOrbitPrototype = {
+    /// given a parameter angle theta find a position on the orbit
+    /// theta is an arbitrary parameter (I think!)
+    sample_ellipse: function(theta) {
+        return this.ellipse.major_vector.clone()
+            .multiplyScalar(Math.cos(theta))
+            .addScaledVector(this.ellipse.minor_vector, Math.sin(theta))
+            .add(this.ellipse.constant);
+    },
+    apoapsis: function() {
+        return this.ellipse.major_vector.clone()
+            .multiplyScalar(-1)
+            .add(this.ellipse.constant);
+    },
+    periapsis: function() {
+        return this.ellipse.major_vector.clone()
+            .add(this.ellipse.constant);
+    },
+    // this isn't exactly right but oh well. computed radius does not visually line up with anything
+    ascending_node: function() {
+        // true_anomaly_at_asc
+        var v = 2 * Math.PI - this.argument_of_periapsis;
+        var r = (this.ang_momentum.lengthSq() / this.mu) / (1 + this.eccentricity * Math.cos(v));
+        return this.node_vector.clone().normalize().multiplyScalar(r);
+    }
+};
+
+/// Find the kepler orbit of b around a.
+///
+/// assume the orbital plane is the XZ plane
+/// for equatorial orbits set the longitude of ascending node to zero
+/// all calculations found on wikipedia (look on each parameter's individual page)
+///
+/// TODO: I believe some of these calculations assume a being at the origin...
+/// TODO: Most calculations appear to break down if we play with the parameters. Needs testing.
+function calculate_kepler_orbit(a, b) {
+    // calculate standard gravitational parameter
+    var mu = G * a.mass;
+    var pos = b.pos;
+    var vel = b.vel;
+    var ang_momentum = b.pos.clone().cross(vel);
+    // I can't believe this calculation works...:
+    // points toward the ascending node from the origin
+    var node_vector = (new THREE.Vector3(0, 1, 0)).cross(ang_momentum);
+
+    // this is the vector between periapsis and apoapsis
+    var eccentricity_vector = pos.clone().multiplyScalar(vel.lengthSq() / mu - 1 / pos.length())
+        .addScaledVector(vel, pos.dot(vel) / mu);
+
+    // our speed is in the thousands of m/s (10^3), so we'll choose our "small number" as 1cm/s
+    var eps = 1e-2;
+    var longitude_of_ascending_node;
+    var argument_of_periapsis;
+    if (Math.abs(vel.y) < eps) {
+        // equatorial orbit... boo special case!
+        longitude_of_ascending_node = 0;
+        argument_of_periapsis = Math.atan2(eccentricity_vector.x, eccentricity_vector.z);
+        node_vector.x = 0;
+        node_vector.y = 0;
+        node_vector.z = 1;
+    } else {
+        // TODO: Is it okay to use ">= zero" for a sign check?
+        longitude_of_ascending_node = Math.acos(node_vector.x / node_vector.length());
+        if (node_vector.z < 0) {
+            longitude_of_ascending_node = 2 * Math.PI - longitude_of_ascending_node;
+        }
+        argument_of_periapsis = Math.acos(node_vector.dot(eccentricity_vector)
+            / node_vector.length() * eccentricity_vector.length());
+        if (eccentricity_vector.y < -eps) {
+            argument_of_periapsis = 2 * Math.PI - argument_of_periapsis;
+        }
+    }
+    var out = Object.create(KeplerOrbitPrototype);
+    var eccentricity = eccentricity_vector.length();
+    var inclination = Math.acos(ang_momentum.y / ang_momentum.length());
+    var specific_energy = vel.lengthSq() / 2 - mu / pos.length();
+    var semi_major_axis = - mu / (2 * specific_energy);
+
+    var longitude_of_periapsis = argument_of_periapsis + longitude_of_ascending_node;
+    if (longitude_of_periapsis > 2 * Math.PI) {
+        longitude_of_periapsis -= 2 * Math.PI;
+    }
+    var dist_from_elliptic_center = semi_major_axis * eccentricity;
+    // get a vector that points toward periapsis
+    // TODO: optimization; my brain isn't working: I think we need to normalize this?
+    var major_vector = eccentricity_vector.clone().normalize();
+    var constant = major_vector.clone().multiplyScalar(-dist_from_elliptic_center)
+        .add(a.pos); 
+    major_vector.multiplyScalar(semi_major_axis);
+    var minor_vector = major_vector.clone()
+        .cross(ang_momentum)
+        .normalize()
+        .multiplyScalar(semi_major_axis * Math.sqrt(1 - eccentricity * eccentricity));
+    Object.assign(out, {
+        node_vector,
+        eccentricity_vector,
+        ang_momentum,
+        mu,
+
+        longitude_of_ascending_node,
+        argument_of_periapsis,
+        eccentricity,
+        inclination,
+        semi_major_axis,
+
+        ellipse: {
+            constant: constant,
+            major_vector,
+            minor_vector,
+        }
+    });
+    return out;
 }
